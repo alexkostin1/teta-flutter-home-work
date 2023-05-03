44f56b4560e65ddcbcb50531894badd2fbe682ef
Костин Алексей <aikostin@mts.ru>
2023-05-03 11:04:00 +0300

    Начало беседы

--- main.dart
+++ main.dart
@@ -1,1 +1,32 @@
-// test
+abstract class Person {
+  var hand = [];
+  void decide() => {};
+}
+
+class Player extends Person {
+  @override
+  void decide() {
+    // TODO: implement decide
+  }
+}
+
+class Diler extends Person {
+  @override
+  void decide() {
+    // TODO: implement decide
+  }
+}
+
+// Класс Deck - описывает колоду карт. Содержит методы "создание колоды", "перетасовка колоды", "взять карту из колоды".
+
+class Deck {
+  void createDeck() => {};
+  void shufflingDeck() => {};
+  void takeCardFromTheDeck() => {};
+}
+
+void main() {
+  print("Beginning of The Game!");
+  Deck newDeck = new Deck();
+  newDeck.createDeck();
+}
