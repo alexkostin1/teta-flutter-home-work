40d94f4cfea32a4607bfee0d847b6a24f4a13bf5
Костин Алексей <aikostin@mts.ru>
2023-05-03 17:47:46 +0300

    добавляю карты

--- main.dart
+++ main.dart
@@ -3,6 +3,25 @@ abstract class Person {
   void decide() => {};
 }
 
+// var Cards = [ // Набор карт
+//     { name: "Ace", weight: 11 },
+//     { name: "King", weight: 10 },
+//     { name: "Queen", weight: 10 },
+//     { name: "Jack", weight: 10 },
+//     { name: "Ten", weight: 10 },
+//     { name: "Nine", weight: 9 },
+//     { name: "Eight", weight: 8 },
+//     { name: "Seven", weight: 7 },
+//     { name: "Six", weight: 6 },
+//     { name: "Five", weight: 5 },
+//     { name: "Four", weight: 4 },
+//     { name: "Three", weight: 3 },
+//     { name: "Two", weight: 2 },
+// ] 
+
+
+// Класс Deck - описывает колоду карт. Содержит методы "создание колоды", "перетасовка колоды", "взять карту из колоды".
+
 class Player extends Person {
   @override
   void decide() {
@@ -17,16 +36,44 @@ class Diler extends Person {
   }
 }
 
-// Класс Deck - описывает колоду карт. Содержит методы "создание колоды", "перетасовка колоды", "взять карту из колоды".
+enum Suit {
+  heart('︎♥'),
+  diamond('♦'),
+  club('♣'),
+  spade('♠');
 
-class Deck {
-  void createDeck() => {};
-  void shufflingDeck() => {};
-  void takeCardFromTheDeck() => {};
+  final String symbol;
+  const Suit(this.symbol);
 }
 
+class Card {
+    String name;
+    int weight;
+    Suit suit;
+
+    Card({ required this.name, required this.weight, required this.suit });
+    @override
+    String toString() => "Name: $name, Weight: $weight, Suit: $suit \n";
+}
+
+class Deck {
+  void createDeck() {
+    print("Колода карт создана!");
+    Suit.values.forEach((suit){
+        globalCards.add(Card(name: "Ace", weight: 11, suit: suit));
+        globalCards.add(Card(name: "King", weight: 10, suit: suit));
+        globalCards.add(Card(name: "Queen", weight: 10, suit: suit));
+    });
+    print(globalCards);
+  }
+  void shufflingDeck(){}
+  void takeCardFromTheDeck(){}
+}
+
+List globalCards = [];
+
 void main() {
-  print("Beginning of The Game!");
-  Deck newDeck = new Deck();
-  newDeck.createDeck();
+    print(" ---= Beginning of The Game! =--- ");
+    Deck newDeck = new Deck();
+    newDeck.createDeck();
 }
